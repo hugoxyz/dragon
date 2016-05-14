f86aa2cd621693ea3ffbe66491b207e4b3b9f07e
hugo <hugohuang1111@gmail.com>
2016-05-14 23:24:26 +0800

    modify

---
+++ dragon/core/CameraComponent.cpp
@@ -0,0 +1,9 @@
+//
+//  CameraComponent.cpp
+//  dragon
+//
+//  Created by hugo on 16/5/12.
+//
+//
+
+#include "CameraComponent.hpp"
---
+++ dragon/core/CameraComponent.hpp
@@ -0,0 +1,14 @@
+//
+//  CameraComponent.hpp
+//  dragon
+//
+//  Created by hugo on 16/5/12.
+//
+//
+
+#ifndef CameraComponent_hpp
+#define CameraComponent_hpp
+
+#include <stdio.h>
+
+#endif /* CameraComponent_hpp */
